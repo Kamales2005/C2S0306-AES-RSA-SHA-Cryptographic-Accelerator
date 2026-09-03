667d468f3450e144f1c66879faac3b3df3f6623c
kamales2005 <kamales5468@gmail.com>
2026-09-03 11:38:48 +0530

    Make GLS and CTS scripts portable

--- gls/zero_delay_simulation.f
+++ gls/zero_delay_simulation.f
@@ -1,7 +1,7 @@
 -gui
 
-/home/scl_shuttle_8_3/tapeout/Crypto_Accelerator_Pad_Synthesis/outputs/C2S0306_incremental.v
-/home/scl_shuttle_8_3/tapeout/Crypto_Accelerator_Pad_Synthesis/tb/top/tb_crypto_synth_top.v
+results/lec/C2S0306_incremental.v
+verification/top/tb_crypto_synth_top.v
 
 /opt/sclpdkv3/scl180/stdcell/fs120/6M1L/verilog/vcs_sim_model/tsl18fs120_scl.v
 /opt/sclpdkv3/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3c01.v
--- physical_design/cts/cts.tcl
+++ physical_design/cts/cts.tcl
@@ -1,27 +1,28 @@
 set init_top_cell C2S0306
-set_global report_timing_format {instance arc net cell slew delay arrival required} 
+set_global report_timing_format {instance arc net cell slew delay arrival required}
 
-source /home/scl_shuttle_8_3/tapeout/pnr/cts/config.tcl
-create_ccopt_clock_tree_spec -file /home/scl_shuttle_8_3/tapeout/pnr/cts/${init_top_cell}_ccopt.spec 
-source /home/scl_shuttle_8_3/tapeout/pnr/cts/${init_top_cell}_ccopt.spec 
-ctd_win -id before_ccopt 
+set cts_dir [file dirname [file normalize [info script]]]
 
-set_ccopt_property -delay_corner max_delay -net_type top   target_max_trans 2 
-set_ccopt_property -delay_corner min_delay -net_type top   target_max_trans 2 
-set_ccopt_property -delay_corner max_delay -net_type trunk target_max_trans 2 
-set_ccopt_property -delay_corner min_delay -net_type trunk target_max_trans 2 
-set_ccopt_property -delay_corner max_delay -net_type leaf  target_max_trans 2 
-set_ccopt_property -delay_corner min_delay -net_type leaf  target_max_trans 2 
+source ${cts_dir}/config.tcl
+create_ccopt_clock_tree_spec -file ${cts_dir}/${init_top_cell}_ccopt.spec
+source ${cts_dir}/${init_top_cell}_ccopt.spec
+ctd_win -id before_ccopt
 
-set_ccopt_property -skew_group MAIN_CLK/all  -delay_corner min_delay target_skew 1.0
-set_ccopt_property  -delay_corner min_delay target_skew 1.0 
+set_ccopt_property -delay_corner max_delay -net_type top   target_max_trans 2
+set_ccopt_property -delay_corner min_delay -net_type top   target_max_trans 2
+set_ccopt_property -delay_corner max_delay -net_type trunk target_max_trans 2
+set_ccopt_property -delay_corner min_delay -net_type trunk target_max_trans 2
+set_ccopt_property -delay_corner max_delay -net_type leaf  target_max_trans 2
+set_ccopt_property -delay_corner min_delay -net_type leaf  target_max_trans 2
+
+set_ccopt_property -skew_group MAIN_CLK/all -delay_corner min_delay target_skew 1.0
+set_ccopt_property -delay_corner min_delay target_skew 1.0
 set_ccopt_property source_driver pc3d01/CIN -clock_tree MAIN_CLK
 
- 
-set_ccopt_property balance_mode full 
-ccopt_design -cts 
-ctd_win -id full_mode 
+set_ccopt_property balance_mode full
+ccopt_design -cts
+ctd_win -id full_mode
 
-report_ccopt_clock_trees -summary -file /home/scl_shuttle_8_3/tapeout/pnr/cts/${init_top_cell}_clock_trees.rpt 
-report_ccopt_skew_groups -summary -file /home/scl_shuttle_8_3/tapeout/pnr/cts/${init_top_cell}_skew_group.rpt 
-reportCongestion -overflow -hotSpot > /home/scl_shuttle_8_3/tapeout/pnr/cts/${init_top_cell}_congestion.rpt
+report_ccopt_clock_trees -summary -file ${cts_dir}/${init_top_cell}_clock_trees.rpt
+report_ccopt_skew_groups -summary -file ${cts_dir}/${init_top_cell}_skew_group.rpt
+reportCongestion -overflow -hotSpot > ${cts_dir}/${init_top_cell}_congestion.rpt
